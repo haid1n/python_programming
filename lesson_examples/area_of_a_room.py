708064a960384f47f9650a4d9a6555ac73c5d0e6
haid1n <ryannab5580@gmail.com>
2024-12-25 11:43:53 -0800

    Add area_of_a_room.py

---
+++ lesson_examples/area_of_a_room.py
@@ -0,0 +1,7 @@
+#Input
+length = float(input("Enter the length of the room in feet: "))
+width = float(input("Enter the length of the room in feet: "))
+#Processing
+area = length * width
+#Output
+print("The area of the room is", area ,"square feet")
