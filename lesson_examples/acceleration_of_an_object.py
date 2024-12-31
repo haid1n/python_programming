5e4ec7e5e0ed5093177fc427769495dbf3c07da1
haid1n <ryannab5580@gmail.com>
2024-12-31 09:59:17 -0800

    Add_acceleration_of_an_object.py

---
+++ lesson_examples/acceleration_of_an_object.py
@@ -0,0 +1,7 @@
+velocity = float(input("Enter the change in velocity = "))
+
+time = float(input("Enter the total time taken = "))
+
+acceleration = velocity / time
+
+print("The acceleration of the object is", acceleration)
