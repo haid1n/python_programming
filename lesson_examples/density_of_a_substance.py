6ccddb64367a26f3801d100d82a07b23de8c8180
haid1n <ryannab5580@gmail.com>
2024-12-31 17:38:57 -0800

    Add density_of_a_substance.py

---
+++ lesson_examples/density_of_a_substance.py
@@ -0,0 +1,7 @@
+mass = float(input("Enter the mass of the substance = "))
+
+volume = float(input("Enter the volume of the substance = "))
+
+density = mass / volume
+
+print("The density of the substance is", density)
