044bde0bfecb311d2d93c5ccbbfd8d6429da9af5
haid1n <ryannab5580@gmail.com>
2024-12-31 17:33:36 -0800

    Add area_of_a_rectangle.py

---
+++ lesson_examples/area_of_a_rectangle.py
@@ -0,0 +1,7 @@
+length = float(input("Enter the length of the rectangle = "))
+
+breadth = float(input("Enter the breadth of the breadth = "))
+
+area = length * breadth
+
+print("The area of a rectangle =" , area)
