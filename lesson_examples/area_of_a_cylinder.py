f68702a32ca7789f11bd92b69bb47ad660dc3912
haid1n <ryannab5580@gmail.com>
2024-12-31 17:32:48 -0800

    Add area_of_a_cylinder.py

---
+++ lesson_examples/area_of_a_cylinder.py
@@ -0,0 +1,7 @@
+radius	=	float(input("Enter the radius of the circle = "))
+
+height	=	float(input("Enter the height of the circle = "))
+
+area	=	2 * 22/7 * radius * (radius + height)
+
+print("The area of the cylinder =" , area)
