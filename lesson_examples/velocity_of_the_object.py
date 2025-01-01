b598dbb05e490fef9ad7e5f487d68f7da3b3caf9
haid1n <ryannab5580@gmail.com>
2024-12-31 17:45:13 -0800

    Add velocity_of_the_object.py

---
+++ lesson_examples/velocity_of_the_object.py
@@ -0,0 +1,7 @@
+displacement = float(input("Enter the displacement of the object = "))
+
+time = float(input("Enter the time taken by the object = "))
+
+velocity = displacement / time
+
+print("The velocity of the object is", velocity)
