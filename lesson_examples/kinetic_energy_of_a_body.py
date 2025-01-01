490602cf9907e0e37a7ac5b7cccb9ceaaa825543
haid1n <ryannab5580@gmail.com>
2024-12-31 17:43:02 -0800

    Add kinetic_energy_of_a_body.py

---
+++ lesson_examples/kinetic_energy_of_a_body.py
@@ -0,0 +1,7 @@
+mass = float(input("Enter the mass of the body = "))
+
+velocity = float(input("Enter the velocity of the body = "))
+
+kinetic = 1/2 * mass * velocity * velocity
+
+print("The kinetic energy of the moving body =", kinetic)
