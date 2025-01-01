c6c2c0ed84949077be723389314f41bc9ccb5218
haid1n <ryannab5580@gmail.com>
2024-12-31 17:39:33 -0800

    Add even_and_odd_number.py

---
+++ lesson_examples/even_and_odd_number.py
@@ -0,0 +1,9 @@
+natural_number = int(input("Enter any natural number :"))
+
+outcome = natural_number % 2
+
+if outcome == 0:
+	print("The number is an even number")
+
+else:
+	print("The number is an odd number")
