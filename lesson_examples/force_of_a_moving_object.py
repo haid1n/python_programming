59a4a2b8c083f0b522eed6772af20eceb02bd84e
haid1n <ryannab5580@gmail.com>
2024-12-31 17:40:06 -0800

    Add force_of_a_moving_object.py

---
+++ lesson_examples/force_of_a_moving_object.py
@@ -0,0 +1,7 @@
+mass = float(input("Enter the mass of the object = "))
+
+acceleration = float(input("Enter the acceleration of the object = "))
+
+force = mass * acceleration
+
+print("The force of the moving object =", force)
