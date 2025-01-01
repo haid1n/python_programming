1e12509b08c19f8139875a9e18ce43695e4b5b04
haid1n <ryannab5580@gmail.com>
2024-12-31 17:43:46 -0800

    Add potential_energy_of_a_body.py

---
+++ lesson_examples/potential_energy_of_a_body.py
@@ -0,0 +1,9 @@
+mass = float(input("Enter the mass of the body = "))
+
+height = float(input("Enter the height of the body = "))
+
+ACCELERATION = 10
+
+potential = mass * ACCELERATION * height
+
+print("The potential energy of the body =", potential)
