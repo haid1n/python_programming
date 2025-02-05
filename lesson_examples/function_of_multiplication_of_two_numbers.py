1073fec7170ac1914a60a070d4c31801d94ba8b4
haid1n <ryannab5580@gmail.com>
2025-02-04 17:36:24 -0800

    Add function_of_multiplication.py

---
+++ lesson_examples/function_of_multiplication_of_two_numbers.py
@@ -0,0 +1,9 @@
+def multiplication_of_two_numbers(first_number, second_number):
+ 
+	product = first_number * second_number
+
+	return product
+
+multiple = multiplication_of_two_numbers(4, 5)
+
+print(multiple)
