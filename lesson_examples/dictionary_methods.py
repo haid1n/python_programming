5a4bb833d6b9972b97fa45157cccb325ad67d27c
haid1n <ryannab5580@gmail.com>
2025-02-04 17:30:56 -0800

    Add dictionary_methods.py

---
+++ lesson_examples/dictionary_methods.py
@@ -0,0 +1,19 @@
+dict_1 = dict() 
+
+dict_2 = dict()
+
+dict_1 = {"names" : "Leon, Joseph, Gordon"}
+
+dict_2 = {"list of numbers" : [1, 3, 5] }
+
+values = dict_2.values()
+
+keys_of_dictionaries = dict_1.keys()
+
+print(values)
+
+print(keys_of_dictionaries)
+
+items = dict_1.items(), dict_2.items()
+
+print(items)
