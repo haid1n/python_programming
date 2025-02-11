c0e34866809fef7943e752fe941be16e61f0709c
haid1n <ryannab5580@gmail.com>
2025-02-10 18:31:53 -0800

    Add string_methods.py

---
+++ lesson_examples/string_methods.py
@@ -0,0 +1,21 @@
+string_1 = "JAMESLUCAS"
+
+string_2 = "Jackson34890"
+
+string_3 = "lonely, happily, genuinely"
+
+capital = string_3.upper()
+
+print(capital)
+
+small = string_1.lower()
+
+print(small)
+
+alphabet = string_1.isalpha()
+
+print(alphabet)
+
+alphanumeric = string_2.isalnum()
+
+print(alphanumeric)
