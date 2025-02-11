1fb06d4ae5cb520f2b6ff96b0a583775b707457a
haid1n <ryannab5580@gmail.com>
2025-02-10 18:31:04 -0800

    Add list_methods.py

---
+++ lesson_examples/list_methods.py
@@ -0,0 +1,23 @@
+list_1 = list()
+
+list_2 = list()
+
+list_1 = ["Anderson", "Ethan", 3, 4, 7]
+
+list_2 = [[1, 2, 3], ("Joshua", "Daniel", "Christian")]
+
+list_3 = list_2.copy()
+
+print(list_3)
+
+list_2.clear()
+
+print(list_2)
+
+list_1.pop(3)
+
+print(list_1)
+
+list_3.remove([1, 2, 3])
+
+print(list_3)
