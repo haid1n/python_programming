a2ff0af6f77702028b9c1fe8c1767f590a6f5c9c
haid1n <ryannab5580@gmail.com>
2025-03-22 03:22:21 -0800

    Update fuvction_of_body_mass_index_wirh_error_handling.py

--- lesson_examples/function_of_body_mass_index_with_error_handling.py
+++ lesson_examples/function_of_body_mass_index_with_error_handling.py
@@ -1,42 +1,47 @@
 def body_mass_index(mass, height):
-	BMI = mass / height * height
+	try:
+		if not str(mass).isnumeric() or not str(height).isnumeric():
+			raise TypeError("The mass or height value is not numeric")
 
-	UNDER_WEIGHT_TRESHOLD = 18.5
+		if mass <= 0 or height <= 0:
+			raise ValueError("The value is less than or equal to zero and therefore not accepted")
 
-	HEALTHY_MINIMUM_TRESHOLD = 18.6
+		BMI = mass / height * height
 
-	HEALTHY_MAXIMUM_TRESHOLD = 24.9
+		UNDER_WEIGHT_TRESHOLD = 18.5
 
-	OVER_WEIGHT_MINIMUM_TRESHOLD = 25
+		HEALTHY_MINIMUM_TRESHOLD = 18.6
 
-	OVER_WEIGHT_MAXIMUM_TRESHOLD = 29.9
+		HEALTHY_MAXIMUM_TRESHOLD = 24.9
 
-	OBESE_TRESHOLD = 30
+		OVER_WEIGHT_MINIMUM_TRESHOLD = 25
 
-	if 0 < BMI < UNDER_WEIGHT_TRESHOLD:
-		print("You are underweight")
+		OVER_WEIGHT_MAXIMUM_TRESHOLD = 29.9
 
-	elif BMI >= HEALTHY_MINIMUM_TRESHOLD and BMI <= HEALTHY_MAXIMUM_TRESHOLD:
-		print("You are healthy")
+		OBESE_TRESHOLD = 30
 
-	elif BMI >= OVER_WEIGHT_MAXIMUM_TRESHOLD and BMI <= OVER_WEIGHT_MAXIMUM_TRESHOLD:
-		print("You are overweight")
+		if 0 < BMI < UNDER_WEIGHT_TRESHOLD:
+			print("You are underweight")
 
-	elif BMI > OBESE_TRESHOLD:
- 		print("You are obese")
+		elif BMI >= HEALTHY_MINIMUM_TRESHOLD and BMI <= HEALTHY_MAXIMUM_TRESHOLD:
+			print("You are healthy")
 
-	if BMI <= 0:
-		raise ValueError("An irregular value")
+		elif BMI >= OVER_WEIGHT_MAXIMUM_TRESHOLD and BMI <= OVER_WEIGHT_MAXIMUM_TRESHOLD:
+			print("You are overweight")
+
+		elif BMI > OBESE_TRESHOLD:
+	 		print("You are obese")
+
+	except Exception as error_message:
+		print(f"Error : {error_message}")
 
 """
 	else:
  		print("The lowest possible BMI is 0.\nYou have a BMI of: ", BMI)
-"""	
-try:
-	body_mass_index(-78, 45)
+"""
 
-except Exception as a:
-	print(f"Error : {a}")
+body_mass_index(7, 3)
+
 
 
 
