27b726fedadbe76fe010e1707c2ed35ae010bcf4
haid1n <ryannab5580@gmail.com>
2025-03-22 02:01:17 -0800

    Create class_year.py

---
+++ lesson_examples/class_year.py
@@ -0,0 +1,21 @@
+class Date:
+	# Constructor method for attributes
+	def __init__(self, day, month, year):
+		# Instance attribute
+		self.day = day
+
+		self.month = month
+
+		self.year = year
+
+	# Method for giving out the year
+	def year(self):
+		return self.year
+
+	# Method for giving out the month
+	def month(self):
+		return self.month
+
+	# Method for giving out the day
+	def day(self):
+		return self.day 
