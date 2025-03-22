face1e854529fef88b347d40cb84da2ae0342521
haid1n <ryannab5580@gmail.com>
2025-03-22 03:28:33 -0800

    Update force_of_gravity_of_a_body_with_error_handling.py

--- lesson_examples/force_of_gravity_of_a_body_with_error_handling.py
+++ lesson_examples/force_of_gravity_of_a_body_with_error_handling.py
@@ -5,16 +5,15 @@ try:
 
 	distance_seperating_the_bodies = float(input("Enter the distance seperating the two bodies = "))
 
+	if mass_of_the_first_body <= 0 or mass_of_the_second_body <= 0 or distance_seperating_the_two_body <= 0:
+		raise ValueError("The value for the mass of the first body or mass of the second body or distance separating the bodies is not accepted")
+
 	GRAVITATIONAL_CONSTANT = 6.67 * 0.00000000001
 
 	gravitation	= GRAVITATIONAL_CONSTANT * mass_of_the_first_body * mass_of_the_second_body / distance_seperating_the_bodies * distance_seperating_the_bodies
 
-	if gravitation <= 0:
-		raise ValueError("Force of gravity is not accepted")
+	print("The force of gravity between the two bodies =", gravitation)
 
-	else:
-		print("The force of gravity between the two bodies =", gravitation)
-
-except Exception as z:
-	print(f"Error : {z}")
+except Exception as error_message:
+	print(f"Error : {error_messages }")
 
