aab6d734a846ce87ab35753a3f2d96e83d15d51f
haid1n <ryannab5580@gmail.com>
2025-05-06 17:58:13 -0800

    Create extracting_file_extension.py

---
+++ lesson_examples/extracting_file_extension.py
@@ -0,0 +1,19 @@
+## Imported modules ##
+from os import listdir
+
+from os.path import isfile , join
+
+
+# Store filepath
+filepath = r"C:\Users\chari plus\Documents"
+
+# Create list of files
+file_list = [f for f in listdir(filepath) if isfile(join(filepath, f))]
+
+# Give each file extension
+for file in file_list:
+	file_components = file.split(".")
+
+	file_extension = file_components[-1]
+
+	print(file_extension)
